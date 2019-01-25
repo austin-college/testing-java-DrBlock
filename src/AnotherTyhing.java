761b589f5679a71f6cc7f8ce0d7be4529871fcbe
Aaron Block <ablock@austincollege.edu>
2019-01-25 17:01:55 -0600

    Notes

--- src/AnotherTyhing.java
+++ src/AnotherTyhing.java
@@ -2,6 +2,7 @@ public class AnotherTyhing {
     public static void main(String[] args) {
         System.out.println("Adding another class");
         //This is the thing that works
+        //This line of code is 100% wrong. You need to fixt htis
     }
 
 
